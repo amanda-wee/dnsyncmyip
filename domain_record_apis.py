bdb522221c513a9590a2bd6e262a2dcef7e327d3
Amanda Wee <amanda@aranel.net>
2018-05-30 12:08:53 +1200

    Added Apache 2.0 license.

--- domain_record_apis.py
+++ domain_record_apis.py
@@ -1,3 +1,23 @@
+"""
+Python 3 API for accessing DNS host APIs to synchronise the actual IP
+address of a host with its domain name A record.
+
+License Notice
+--------------
+Copyright 2018 Amanda Wee
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import requests
 
 
